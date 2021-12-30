4b7110693c975239a09fb51fa8ba6446f759696b
Graham Pentheny <graham.pentheny@gmail.com>
2021-12-30 16:42:37 -0500

    Added hotloading toggle and interval check seconds to Settings

--- Assets/DarkConfig/Settings.cs
+++ Assets/DarkConfig/Settings.cs
@@ -22,5 +22,15 @@ namespace DarkConfig {
 		/// Default options for refication.  Change this if you want to change
         /// DarkConfig behavior without passing in parameters to each call.
         public ReificationOptions DefaultReifierOptions = ReificationOptions.AllowMissingExtraFields | ReificationOptions.CaseSensitive;
+
+		/// If enabled DarkConfig will scan files for changes every HotloadCheckInterval seconds.
+		/// Setting it to false stops hotloading.  Useful during production when configs are under rapid iteration.
+		public bool EnableHotloading {
+			get => Config.FileManager.IsHotloadingFiles;
+			set => Config.FileManager.IsHotloadingFiles = value;
+		}
+		
+		/// How often, in seconds, to scan files for changes.
+		public float HotloadCheckFrequencySeconds = 2f;
 	}
 }
