676c6d9c4c337084c1ab80fd72cd3eb2bd433c46
Graham Pentheny <graham.pentheny@gmail.com>
2022-01-18 09:02:37 -0500

    Moved log verbosity setting to Settings

--- Assets/DarkConfig/Settings.cs
+++ Assets/DarkConfig/Settings.cs
@@ -18,10 +18,19 @@ namespace DarkConfig {
         CaseSensitive = 1 << 2
     }
 
+    public enum LogVerbosity {
+        Error,
+        Warn,
+        Info
+    }
+
 	public class Settings {
 		/// Default options for refication.  Change this if you want to change
         /// DarkConfig behavior without passing in parameters to each call.
         public ReificationOptions DefaultReifierOptions = ReificationOptions.AllowMissingExtraFields | ReificationOptions.CaseSensitive;
+
+        /// How aggressively DarkConfig logs
+        public LogVerbosity LogLevel = LogVerbosity.Info;
 
 		/// If enabled DarkConfig will scan files for changes every HotloadCheckInterval seconds.
 		/// Setting it to false stops hotloading.  Useful during production when configs are under rapid iteration.
