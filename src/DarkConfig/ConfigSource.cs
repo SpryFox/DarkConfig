ab6d4daabb79ba80465903b4195b461293176e7c
Graham Pentheny <graham.pentheny@gmail.com>
2022-04-04 01:58:44 -0400

    Added some docstrings

--- src/DarkConfig/ConfigSource.cs
+++ src/DarkConfig/ConfigSource.cs
@@ -5,14 +5,18 @@ namespace DarkConfig {
     /// A source of config files to load.
     /// e.g. a folder on disk, a binary file, a web server, etc.
     public abstract class ConfigSource {
+        /// Does this config source support hotloading config files?
         public abstract bool CanHotload { get; }
 
+        /// Find and load all configs that this source knows about
         public abstract void Preload();
 
+        /// Try to hotload config files.  Adds names of changed files to the <paramref name="changedFiles"/> list.
         public virtual void Hotload(List<string> changedFiles) {}
         
         /////////////////////////////////////////////////
         
+        /// All the currently loaded config file data.
         public Dictionary<string, ConfigFileInfo> AllFiles = new Dictionary<string, ConfigFileInfo>();
     }
 }
