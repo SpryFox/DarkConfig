eb204f4060c071de34d8e6359a4992ee94622579
Francois van Niekerk <flash.slash@gmail.com>
2022-09-14 00:31:39 +0200

    Improve FileSource when used in conjunction with other sources with SetModifiedTimeOnChecksumMatch

--- src/DarkConfig/FileSource.cs
+++ src/DarkConfig/FileSource.cs
@@ -8,6 +8,8 @@ namespace DarkConfig {
     /// Uses file modified timestamps to decide whether it should hotload or not.
     public class FileSource : ConfigSource {
         public override bool CanHotload { get; }
+        /// If true, we till update the file modified time on disk with a checksum match, so hotloading won't check again next session
+        public bool SetModifiedTimeOnChecksumMatch = false;
                 
         ////////////////////////////////////////////
 
@@ -77,23 +79,34 @@ namespace DarkConfig {
                 
                 using (var fileStream = File.OpenRead(file)) {
                     int checksum = Internal.ChecksumUtils.Checksum(fileStream);
-                    fileStream.Seek(0, SeekOrigin.Begin);
-                    
-                    // Update the modified timestamp and file size even if the checksum is the same.
-                    // Because we didn't early out a few lines above, we know that at least one of these values
-                    // is stale.
-                    fileInfo.Modified = modified;
-                    fileInfo.Size = fileSize;
                     
                     if (checksum == fileInfo.Checksum) {
-                        // The files are identical.
+                        // The files are identical (according to the checksum)
+                        // Update the file size and modified timestamp
+                        // We didn't early out a few lines above, so we know that at least one of these values is stale.
+                        fileInfo.Size = fileSize;
+                        if (SetModifiedTimeOnChecksumMatch) {
+                            // Update the modified time on disk so we don't re-check this next session
+                            try {
+                                fileStream.Close();
+                                File.SetLastWriteTimeUtc(file, fileInfo.Modified);
+                                Configs.LogInfo($"Set mtime of file with matching checksum {fileInfo}");
+                            } catch (Exception e) {
+                                Configs.LogWarning($"Error setting mtime of file {fileInfo}: {e}"); // Warning cause execution can safely continue
+                                fileInfo.Modified = modified; // At least we won't reload this session
+                            }
+                        } else {
+                            fileInfo.Modified = modified;
+                        }
                         continue;
                     }
                     
                     // File has changed. Hotload it.
+                    fileStream.Seek(0, SeekOrigin.Begin);
+                    fileInfo.Parsed = Configs.LoadDocFromStream(fileStream, file);
                     fileInfo.Checksum = checksum;
                     fileInfo.Modified = modified;
-                    fileInfo.Parsed = Configs.LoadDocFromStream(fileStream, file);
+                    fileInfo.Size = fileSize;
                     
                     changedFiles.Add(fileName);
                 }
